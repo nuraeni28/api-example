2edf8ec9ed39dd71824d2db7dd87ffc2865c5cb3
Nuraeni <nuraeniexecutive18@gmail.com>
2023-01-04 17:27:55 +0800

    add middleware validation register

---
+++ src/middleware/validation.middleware.js
@@ -0,0 +1,29 @@
+const { check, validationResult } = require('express-validator');
+const { User } = require('../models/index');
+
+exports.runValidation = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      status: 'failed',
+      errors: errors.array(),
+    });
+  }
+  next();
+};
+
+exports.validationRegister = [
+  check('name', 'Name cannot empty').notEmpty(),
+  check('phone').notEmpty().withMessage('Phone cannot empty')
+    .matches(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+    .withMessage('Your number is not valid'),
+  check('email').notEmpty().withMessage('Email cannot empty')
+    .isEmail()
+    .withMessage('Your email is not valid'),
+  check('password').notEmpty().withMessage('Password cannot empty')
+    .isLength({ min: 8 })
+    .withMessage('password must be at last 8 characters')
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/)
+    .withMessage('Password must be contain at least one uppercase, at least one lower case and  at least one special character'),
+  check('address', 'Address cannot empty').notEmpty(),
+];
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 const {register} = require('../controllers/auth.controllers');
+const { validationRegister, runValidation } = require('../middleware/validation.middleware');
 
-router.post('/register', register);
+router.post('/register', validationRegister, runValidation, register);
 
 module.exports = router;
